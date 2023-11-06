3bb3562542250348a5746759567d5b4e485505e4
Shubham-Rasal <bluequbits@gmail.com>
2023-11-06 22:15:07 +0530

    Add config file and load config in main function.

--- main.go
+++ main.go
@@ -6,22 +6,24 @@ import (
 
 	"github.com/Shubham-Rasal/blog-backend/api"
 	db "github.com/Shubham-Rasal/blog-backend/db/sqlc"
+	"github.com/Shubham-Rasal/blog-backend/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:password@localhost:5432/blog?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	DB, err := sql.Open(dbDriver, dbSource)
+
+	//load config
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load env", err)
+	}
+
+	DB, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db", err)
 	}
 	store := db.NewStore(DB)
 	server := api.NewServer(*store)
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 }
