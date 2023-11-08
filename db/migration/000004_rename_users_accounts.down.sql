1508aee7710e270116801005da90cc5572b2122e
Shubham-Rasal <bluequbits@gmail.com>
2023-11-08 23:21:21 +0530

    Rename users table to accounts and add user_id column

--- db/migration/000004_rename_users_accounts.down.sql
+++ db/migration/000004_rename_users_accounts.down.sql
@@ -1,2 +1,6 @@
+
+ALTER TABLE "accounts" DROP CONSTRAINT "accounts_user_id_fkey";
+DROP TABLE IF EXISTS "users";
+ALTER TABLE "accounts" DROP COLUMN "user_id";
 ALTER TABLE "accounts" RENAME TO "users";
 
--- db/migration/000004_rename_users_accounts.up.sql
+++ db/migration/000004_rename_users_accounts.up.sql
@@ -1,21 +1,14 @@
 ALTER TABLE "users" RENAME TO "accounts";
+ALTER TABLE "accounts" ADD COLUMN "user_id" integer unique;
 
--- create a new table users with the following columns
--- id, username, password, email
--- id is a serial primary key
--- username is a varchar not null
--- password is a varchar not null
--- email is a varchar not null
--- ALTER TABLE "accounts" ADD COLUMN "user_id" integer;
-
--- CREATE TABLE "users" (
---   "id" bigserial PRIMARY KEY,
---   "username" varchar NOT NULL,
---   "password" varchar NOT NULL,
---   "email" varchar NOT NULL
--- );
+CREATE TABLE "users" (
+  "id" bigserial PRIMARY KEY,
+  "username" varchar NOT NULL,
+  "password" varchar NOT NULL,
+  "email" varchar NOT NULL
+);
 
 -- -- add a constraint into account fk with user_id
--- ALTER TABLE "accounts" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "accounts" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
 
